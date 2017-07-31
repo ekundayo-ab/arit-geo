4d499f82ef3d628cd890a076a656d68df4303b80
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-07-31 16:22:13 +0100

    implemented logic for detecting if input supplied to aritgeo is arithmetic

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+module.exports = aritGeo;
+
+function aritGeo(seq){
+	if (seq.length == 0){
+		return 0;
+	}
+
+	for (let i=0; i < seq.length; i++){
+
+		if (seq[i-1] == undefined || seq[i+1] == undefined) {
+			continue;
+		}
+
+		/*if (typeof(seq[i] != "number")){
+			return -1;
+		}*/
+
+		if (seq[i] % seq[i-1] !== 0){
+
+			// Test for arithmetic sequence
+			let ariCheck = [];
+			for (let i=0; i < seq.length; i++){
+				if (seq[i-1] == undefined || seq[i+1] == undefined) {
+					continue;
+				}
+				ariCheck.push(seq[i] - seq[i-1]);
+			}
+
+			let ariVar = 0;
+			for (let i=0; i<ariCheck.length; i++){
+				 if (ariCheck[i] == ariCheck[0]){
+				 	return "Arithmetic";
+				 } else {
+				 	return -1;
+				 }
+			}
+
+		} else if (seq[i+1] % seq[i] == 0) {
+
+			
+			
+		}
+
+	}
+
+
+}
