8940405eabc4b6f08636914f3cf365258e748a4b
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-07-31 16:19:07 +0100

    added test folder to test containing test file to test program in app.js with mocha and chai

---
+++ test/appTest.js
@@ -0,0 +1,67 @@
+const assert = require('chai').assert;
+const aritGeo = require('../app.js');
+
+describe("aritGeo", function() {
+
+	describe("check if input is arithmetic sequence", function(){
+		it("should return Arithmetic", function(){
+			assert.equal(aritGeo([1,2,3,4,5,6]), 'Arithmetic');
+		});
+	});
+
+	describe("check if input is geometric sequence", function(){
+		it("should return Geometric", function(){
+			assert.equal(aritGeo([1,2,4,8,16,32]), 'Geometric');
+		});
+	});
+
+	describe("check if input is an empty array", function(){
+		it("should return zero", function(){
+			assert.equal(aritGeo([]), 0);
+		});
+	});
+
+	describe("check if input is a sequence", function(){
+		it("should return -1", function(){
+			assert.equal(aritGeo([1,9,3,4,5,17]), -1);
+		});
+	});
+
+	describe("check if input type is a string", function(){
+		it("should return -1", function(){
+			assert.typeOf(aritGeo("a sequence"), "string");
+		});
+	});
+
+	describe("check if input type is a number", function(){
+		it("should return -1", function(){
+			assert.typeOf(aritGeo(1), "number");
+		});
+	});
+
+	describe("check if input is a special character", function(){
+		it("should return -1", function(){
+			assert.equal(aritGeo("$&^"), -1);
+		});
+	});
+
+	describe("check if input is null", function(){
+		it("should return -1", function(){
+			assert.equal(aritGeo(null), -1);
+		});
+	});
+
+	describe("check if no argument was supplied", function(){
+		it("should return -1", function(){
+			assert.equal(aritGeo(undefined), -1);
+		});
+	});
+
+	describe("check if input is a special character", function(){
+		it("should return -1", function(){
+			assert.equal(aritGeo(), -1);
+		});
+	});
+
+});
+
