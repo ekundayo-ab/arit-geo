2dda4a5f7666a70ac1d322cd4dda4e590222902e
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-01 09:41:17 +0100

    Refactoring of code base for to catch all test cases
    function aritGeo was refactored to catch all test cases,
    a helper function was used to check if all values in
    "ariCheck" array or "geoCheck" array ar common differences,
    
    if "ariCheck" values are then sequence is - Arithmetic,
    while if "geoCheck" values are sequence is - Geometric,
    
    - if blocks was used in catching not expected input
    - one helper function was used "sameValues()"

--- app.js
+++ app.js
@@ -1,66 +1,40 @@
-
-
 module.exports = aritGeo;
 
 function aritGeo(seq){
-	if (seq.length == 0){
+
+	let ariCheck = [];
+	let geoCheck = [];
+
+	if (Array.isArray(seq) && seq[0] == undefined){
 		return 0;
 	}
 
-	for (let i=0; i < seq.length; i++){
+	if (!Array.isArray(seq) || seq.length < 2 || seq == undefined){
+		return -1
+	}
 
-		if (seq[i-1] == undefined || seq[i+1] == undefined) {
-			continue;
-		}
+	for (let i=1; i < seq.length; i++){
 
-		/*if (typeof(seq[i] != "number")){
+		if (seq[i] - seq[i-1] == 0) {
 			return -1;
-		}*/
-
-		if (seq[i] % seq[i-1] !== 0){
-
-			// Test for arithmetic sequence
-			let ariCheck = [];
-			for (let i=0; i < seq.length; i++){
-				if (seq[i-1] == undefined || seq[i+1] == undefined) {
-					continue;
-				}
-				ariCheck.push(seq[i] - seq[i-1]);
-			}
-
-			let ariVar = 0;
-			for (let i=0; i<ariCheck.length; i++){
-				 if (ariCheck[i] == ariCheck[0]){
-				 	return "Arithmetic";
-				 } else {
-				 	return -1;
-				 }
-			}
-
-		} else if (seq[i+1] % seq[i] == 0) {
-
-			// Test for geometric sequence
-			let geoCheck = [];
-
-			for (let i=0; i < seq.length; i++){
-				if (seq[i-1] == undefined || seq[i+1] == undefined) {
-					continue;
-				}
-				geoCheck.push(seq[i] / seq[i-1]);
-			}
-
-			let geoVar = "";
-			for (let i=0; i<geoCheck.length; i++){
-				 if (geoCheck[i] == geoCheck[0]){
-				 	return "Geometric";
-				 } else {
-				 	return -1
-				 }
-			}
-			
 		}
+
+		ariCheck.push(seq[i] - seq[i-1]);
+		geoCheck.push(seq[i] / seq[i-1]);
 
 	}
 
+	if (sameValues(ariCheck)){
+		return "Arithmetic";	
+	} else if (sameValues(geoCheck)){
+		return "Geometric";
+	}
 
+	return -1;
+	
 }
+
+function sameValues(arr) {
+  	return arr.every( (v,i,a) => v===a[0] );
+}
+
